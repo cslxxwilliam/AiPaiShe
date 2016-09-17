040c4d5aae61889afec7116186d47ba785d31565
Hillmon <hillmon919@gmail.com>
2016-09-18 01:05:21 +0800

    Refactor src folder to put source code files under "java" folder by conventions. Update aipaishe-home HTML to include the search tool bar and footer. Add a new main application class for AiPaiShe.

---
+++ src/main/java/aipaishe/AipaisheAppliation.java
@@ -0,0 +1,19 @@
+package aipaishe;
+
+import hello.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+/**
+ * Created by hillmon on 18/9/2016.
+ */
+@SpringBootApplication
+public class AipaisheAppliation {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+}
