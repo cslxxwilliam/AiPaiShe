19e9ca91703e77673d8e6cab5f0d6e62deda1734
cslxxwilliam <cslxxwilliam@gmail.com>
2017-06-04 17:50:59 +0800

    AIPAISHE-58 add fake event viewing page - to add the bottom section for image uploading and displaying

---
+++ public/eventviewer.js
@@ -0,0 +1,10 @@
+angular.module('EventViewerApp', ['ngMaterial'])
+    .controller('BasicDemoCtrl', BasicDemoCtrl)
+    .controller('PanelDialogCtrl', PanelDialogCtrl);
+
+function BasicDemoCtrl($mdPanel, $scope) {
+  $scope.readonly=true;
+  $scope.eventDate = new Date("2017-06-17");
+  $scope.eventName = "Shenzhen APS Party";
+  $scope.eventVenue = "Binhe Dadao No.179";
+};
