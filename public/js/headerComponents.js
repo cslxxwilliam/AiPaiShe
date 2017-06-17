ab82e5bd5d366fdece0d5ee3ea952b9d2abb2f8e
Hillmon <hillmon919@gmail.com>
2017-06-18 01:24:55 +0800

    refactor the header to be an Angular component

---
+++ public/js/headerComponents.js
@@ -0,0 +1,19 @@
+var aipaisheApp = angular.module('aipaisheApp');
+
+var headerComponent = {
+    bindings: {},
+    templateUrl: 'header.html',
+    controller: HeaderComponentController,
+    controllerAs: 'vm'
+};
+
+aipaisheApp.component('aipaisheHeader', headerComponent);
+
+HeaderComponentController.$inject = ['$scope'];
+
+function HeaderComponentController($scope) {
+    var vm = this;
+
+    this.username = "Pleae login...";
+
+}
